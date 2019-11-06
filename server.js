60d517ed76b15c852fba17e0d79cd3f9956449ce
Glitch (hello-express) <none>
2019-11-06 03:06:54 +0000

    🐛🕷 Checkpoint ./server.js:2543762/73

--- server.js
+++ server.js
@@ -77,9 +77,9 @@ fs.readdir('./events/', (err, files) => { // We use the method readdir to read w
 
 
 
-client.on("guildMemberAdd", (member) => {
+bot.on("guildMemberAdd", (member) => {
   console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.find(c => c.name === "welcome").send(`"${member.user.username}" has joined this server`);
+  member.guild.channels.find(c => c.name === "ɷ┊chatsy♡ ").send(`Welcome!`);
 });
 
 
