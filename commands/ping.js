b2be9366219a01b1983a03234c3f0ec51bf50452
Glitch (hello-express) <none>
2019-11-07 21:56:27 +0000

    🍙🎽 Checkpoint ./commands/sexy-ping.js:2543762/569 ./commands/ping.js:2543762/306

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
      
-  message.channel.send('You are a whore: ' + Math.round(bot.ping))
+  const exampleEmbed = new Discord.RichEmbed()
+    
+    .setColor('#0099ff')
+    .setTitle('Pong! :ping_pong:')
+    .setDescription('> Ping: ' + Math.round(bot.ping))
   
-  let guildname = message;
+  
+  message.channel.send(exampleEmbed);
   
   console.log(`Ping used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)
 }
--- commands/sexy-ping.js
+++ commands/sexy-ping.js
@@ -3,8 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   const exampleEmbed = new Discord.RichEmbed()
-    .setTitle('Ping:')
-    .setDescription(Math.round(bot.ping))
+    
+    .setColor('#0099ff')
+    .setTitle('Pong! :ping_pong:')
+    .setDescription('> Ping: ' + Math.round(bot.ping))
   
   
   message.channel.send(exampleEmbed);
