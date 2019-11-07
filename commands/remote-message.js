440b6fd67dd61e458e50ff1ed41a65ca46f33efc
Glitch (hello-express) <none>
2019-11-07 02:36:05 +0000

    🎐🎌 Checkpoint ./commands/remote-message.js:2543762/849

--- commands/remote-message.js
+++ commands/remote-message.js
@@ -5,7 +5,12 @@ module.exports.run = async (bot, message, args) => {
   let ownerid = 171335165476470784;
   
   if(!message.author.id === ownerid) return;
-    if(!message.channel.type === dm)
+    if(!message.guild === null) return;
+  
+        let target = args.slice(0).join(' ');
+        
+  
+        message.target.send(`${message}`);
   
   
   
@@ -18,5 +23,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name: "ping"
+  name: "rm"
 }
