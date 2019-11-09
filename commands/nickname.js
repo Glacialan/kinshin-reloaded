40ca31ebf24311d685d9879888b0e88e29f2883b
Glitch (hello-express) <none>
2019-11-09 02:53:40 +0000

    👞🌴 Checkpoint ./commands/nickname.js:2543762/1488

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,29 +1,21 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
 module.exports.run = async (bot, message, args) => {
-    
-  if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.member.setNickname(message.content.replace());
+   if (message.author.hasPermission('MANAGE_NICKNAMES'))
+      message.member.setNickname(message.content.replace('k.setmynickname ', ''))
+      let newnick = message.content.replace('k.setmynickname ', '');
+      message.channel.send('> I updated changed your nickanme to ' + newnick)
   
   
-  const taggedUser = message.mentions.users.first();
   
   
   
-  const exampleEmbed = new Discord.RichEmbed()
-  
-    .setColor(config.mainColor)
-    .setTitle('Pong! :ping_pong:')
-    .addField('> **Ping: **' + Math.round(bot.ping), '\u200b', true)
-    .setTimestamp()
-    .setFooter('Probably accurate...')  
-  
-  message.channel.send(exampleEmbed);
+
   console.log(`Nickname used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)
 }
 
 module.exports.help = {
-  name: "nickname"
+  name: "setmynickname"
 }
 
 
