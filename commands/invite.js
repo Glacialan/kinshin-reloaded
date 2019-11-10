17b5bd45802d90b93165ed8b4f8ec81491752a85
Glitch (hello-express) <none>
2019-11-10 00:25:02 +0000

    🍗🌽 Checkpoint ./commands/vibecheck.js:2543762/34 ./commands/invite.js:2543762/1218

---
+++ commands/invite.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+module.exports.run = async (bot, message, args) => {
+  const exampleEmbed = new Discord.RichEmbed()
+     
+    .setColor(config.mainColor)
+    .setTitle("Invite Kinshin")
+    .addField("\u200b", "Add Kinshin to your server! [Click here](https://discordapp.com/oauth2/authorize?client_id=513587870704074752&scope=bot&permissions=8)", true)
+    .setTimestamp()
+    .setThumbnail(bot.user.avatarURL)
+    .addBlankField()
+    
+  
+  message.channel.send(exampleEmbed);
+  console.log(`Invite used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)    
+}
+
+module.exports.help = {
+  name: "invite"
+}
