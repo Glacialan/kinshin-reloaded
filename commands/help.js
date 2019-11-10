c074e736b3da91f9c18a608f25490df3d799ba4f
Glitch (hello-express) <none>
2019-11-10 02:36:02 +0000

    🎹🐿 Checkpoint ./commands/help.js:2543762/579

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,17 @@
 const Discord = require("discord.js");
-
+const config = require("../config.json");
 module.exports.run = async (bot, message, args) => {
 
+    const exampleEmbed = new Discord.RichEmbed()
   
+    .setColor(config.mainColor)
+    .setTitle('Kinshin Help
+    .addField('**ping**'.'/u200b', true)
+    .setTimestamp()
+    .setFooter('Created by Glacialan#2576')  
   
-  
-  console.log(`Ping used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)
+  message.channel.send(exampleEmbed);
+  console.log(`Help used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)
 
 }
 
