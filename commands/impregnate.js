571710bdd5856b17570559bfc9c4c1f9ea8126ee
Glitch (kinshin) <none>
2019-11-18 03:01:47 +0000

    ⛴🛳 Checkpoint ./commands/impregnate.js:2543762/646 ./server.js:2543762/58

--- commands/impregnate.js
+++ commands/impregnate.js
@@ -3,14 +3,15 @@ const config = require("../config.json");
 module.exports.run = async (bot, message, args) => {
   
   
-  const options = ["Sexy Time", ]
+  const options = ["Sexy Time", "Doing The Dirty", "Cuddle Puddle", "OwO What's This?"]
   const title = options[Math.floor(Math.random()*options.length)];
   var member =  message.mentions.members.first();  
   var author = message.author
   const exampleEmbed = new Discord.RichEmbed()
 
     .setColor(config.mainColor)
-    .setTitle('😏 Sexy Time')
+    .setTitle('😏 ' + title)
+    .setThumbnail("https://66.media.tumblr.com/48353c09e0d627565feeeb7f3843d1e4/tumblr_phwisbTrjg1u5vwmu_400.gif")
     .setDescription(member + ' was impregnated by ' + author)
     .setFooter('Why is this a thing...')
     .setTimestamp()
