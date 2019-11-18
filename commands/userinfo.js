b65485957d64e27c64ed8c6ab917382203e8e20d
Glitch (kinshin) <none>
2019-11-18 04:01:48 +0000

    ⏲🍍 Checkpoint ./commands/userinfo.js:2543762/991

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,7 +3,14 @@ const config = require("../config.json");
 module.exports.run = async (bot, message, args) => {
   
   //.format("dddd, MMMM Do YYYY, HH:mm")
+  function checkDays(date) {
+      let now = new Date();
+      let diff = now.getTime() - date.getTime();
+      let days = Math.floor(diff / 86400000);
+      return days + (days == 1 ? " day" : " days") + " ago";
+  };
   
+  let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
   var member =  message.mentions.members.first();  
   var author = message.author
   
@@ -13,16 +20,14 @@ module.exports.run = async (bot, message, args) => {
 
     .setColor(config.mainColor)
     .setTitle('📄 User Info:')
-    .setDescription(member)
-  
-    
+    .setDescription('Display Name: ' + member)
+    .addField('Account Age:', `${member.user.createdAt.toUTCString().substr(0, 16)} (${checkDays(member.user.createdAt)})`)
+    .addField('Join Date:', `${member.guild.joinDate}`)
+    .addField('Roles:', member.roles ? member.roles.map(r => `${r}`).join(' | ') : "", true)
+    .setThumbnail(member.avatar)
     .setTimestamp()
   
-
   if(member);
-    
-
-   
     message.channel.send(exampleEmbed);
     console.log(`User Info used in ${message.guild.name} #${message.channel.name} by ${message.author.tag}`)
 
